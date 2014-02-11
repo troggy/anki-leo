1abf59d60490ff2c5dfb2d3218c17fffee4e40f9
troggy <7r0ggy@gmail.com>
2014-02-11 12:30:28 +0300

    using DOM as datasource. That's it: all shown words will be exported

--- src/leo-export.js
+++ src/leo-export.js
@@ -1,22 +1,22 @@
-var createExportButton = function(dict) {
+var createExportButton = function(data) {
 	$("<a class='btn'>Экспорт</a>").appendTo("div.dict-title-inner").click(function() {
-		var wordsList = dict.wordsCat.reduce(function(wordsList, category, i) { 
-				return wordsList.concat(category.words.map(function(word) {
-					 return word.word_value + ';' + word.user_translates[0].translate_value + ';' + word.user_translates[0].picture_url
-		 		}));
-			}, new Array()).join('\n');
-		
+		var wordsCSVList = $('.dict-item-word').map(function(i, item) {
+				return $(item).data("word-value") + ';' + $(item).find(".translates").text().trim() + ';http:' + $(item).find(".item-word-img").attr("src"); 
+			});
+
+		var wordsCSVString = $.makeArray(wordsCSVList).join('\n')
 		saveAs(
-			new Blob([wordsList], {type: "text/plain;charset=utf-8"}),
+			new Blob([wordsCSVString], {type: "text/plain;charset=utf-8"}),
 			'lingualeo-dict-export.txt'
 		);
+		alert('Exported words: ' + wordsCSVList.length + ' of ' + data.count_words);
 	});
 };
 
 
 // inject script in page that will send dictionary data using portMessage
 var s = document.createElement('script');
-s.textContent = "window.postMessage({ type: 'LeoDict', payload: CONFIG.pages.userdict3 }, '*');"
+s.textContent = "window.postMessage({ type: 'LeoDict', payload: { count_words: CONFIG.pages.userdict3.count_words } }, '*');"
 s.onload = function() {
     this.parentNode.removeChild(this);
 };
