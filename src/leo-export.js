9c4db56ac9515f5f7f74d59ac9b9e8839641e66c
Kosta Korenkov <7r0ggy@gmail.com>
2015-01-14 10:13:31 +0800

    Export all contextual translations

--- src/leo-export.js
+++ src/leo-export.js
@@ -15,9 +15,13 @@ var showToolTip = function(message, style) {
 };
 
 var wordsToCSV = function(words) {
-	return words.reduce(function(wordsList, category, i) { 
+	return words.reduce(function(wordsList, category, i) {
 				return wordsList.concat(category.words.map(function(word) {
-	 					 return word.word_value + ';' + word.user_translates[0].translate_value.replace(/;/g, '.')
+ 						 var translations = word.user_translates.map(function(t) { 
+  					       return t.translate_value.replace(/;/g, '.');
+				         }).join(", ");
+
+	 					 return word.word_value + ';' + translations
 	 					  + ';http:' + word.user_translates[0].picture_url + ';' + word.transcription + ';' 
 	 					  + (word.context ? word.context.replace(/;/g, '.').replace(/\n/, '').replace('"', "'") : '');
 		 		 		}));
