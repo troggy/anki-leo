c327646dd5b18d2096add75629a001f5dd0c660e
Kosta Korenkov <kosta@whoot.me>
2019-05-16 10:26:56 +0300

    fix: tests for dictPageConfig

--- src/js/dictPageConfig.js
+++ src/js/dictPageConfig.js
@@ -3,6 +3,5 @@ const dictPageRE = new RegExp('https?://lingualeo.com/(.+?)/dictionary/vocabular
 export default (url) => {
   const match = url.match(dictPageRE)
   if (!match) return
-  console.log({ localeName: match[1], wordGroup: parseInt(match[2]) || 1 })
   return { localeName: match[1], wordGroup: parseInt(match[2]) || 1 }
 }
--- src/js/dictPageConfig.test.js
+++ src/js/dictPageConfig.test.js
@@ -8,12 +8,24 @@ describe('dictPageConfig', t => {
       { 
         url: 'https://lingualeo.com/ru/dictionary/vocabulary/my', 
         expected: { localeName: 'ru', wordGroup: 1 }
+      },
+      { 
+        url: 'https://lingualeo.com/ru/dictionary/vocabulary/12', 
+        expected: { localeName: 'ru', wordGroup: 12 }
+      },
+      { 
+        url: 'http://lingualeo.com/ru/dictionary/vocabulary/12', 
+        expected: { localeName: 'ru', wordGroup: 12 }
+      },
+      { 
+        url: 'https://lingualeo.com/ru/dashboard', 
+        expected: undefined
       }
     ]
 
     tests.forEach(fxt => {
       t.test('should ' + (fxt.expected === false ? 'NOT ' : '') + 'match ' + fxt.url, t => {
-        t.equal(dictPageConfig(fxt.url), fxt.expected)
+        t.deepEqual(dictPageConfig(fxt.url), fxt.expected)
       })
     })
   })
